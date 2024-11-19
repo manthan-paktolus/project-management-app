ee144e10c3dfe08b63550fdc6088602104090d0b
Your Name <you@example.com>
2024-11-19 15:52:49 +0530

    Tasks Api implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 /* ROUTE IMPORTS */
 
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
