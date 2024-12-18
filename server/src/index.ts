a1dd6cad2253956ca2b7fa95599b21bb6d73495b
Your Name <you@example.com>
2024-12-18 20:32:02 +0530

    user routes backend to get all users api implementated

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import taskRoutes from "./routes/taskRoutes";
+import userRoutes from "./routes/userRoutes";
 
 /* ROUTE IMPORTS */
 
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
